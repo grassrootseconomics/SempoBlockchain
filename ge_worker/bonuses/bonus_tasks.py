22c651d1be5d1f61f79f2f10f8945c4406a949e3
philip <philipwafula2@gmail.com>
2020-03-26 14:34:23 +0300

    Refactors import.

--- ge_worker/bonuses/bonus_tasks.py
+++ ge_worker/bonuses/bonus_tasks.py
@@ -1,16 +1,8 @@
-import os
-import sys
-
-parent_directory = os.path.abspath(os.path.join(os.getcwd(), "..", ".."))
-sys.path.append(parent_directory)
-sys.path.append(os.getcwd())
-
-
-from ge_worker.bonuses import celery_app
-from ge_worker.bonuses.bonus_processor import BonusProcessor
+from bonuses.utils import celery_app
+from bonuses.bonus_processor import BonusProcessor
 
 
 @celery_app.task()
 def auto_disburse_daily_bonus():
     bonus_processor = BonusProcessor(issuable_amount=1000)
-    return bonus_processor.auto_disburse_daily_bonus()
+    return bonus_processor.auto_disburse_amount()
