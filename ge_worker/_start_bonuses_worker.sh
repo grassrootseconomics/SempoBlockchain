e8eb61d072868135b6a7d9eb5bbe5ca115efc35e
Philip Wafula <philipwafula2@gmail.com>
2020-04-08 02:51:01 +0300

    Removes ge_worker

--- ge_worker/_start_bonuses_worker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-celery -A bonuses worker --loglevel=INFO
--- ge_worker/bonuses/bonus_tasks.py
+++
@@ -1,8 +0,0 @@
-from bonuses.utils import celery_app
-from bonuses.bonus_processor import BonusProcessor
-
-
-@celery_app.task()
-def auto_disburse_daily_bonus():
-    bonus_processor = BonusProcessor(issuable_amount=1000)
-    return bonus_processor.auto_disburse_amount()
