c483ac80bd02064d6b3c652d9fa9d291b0ac31ac
Philip Wafula <philipwafula2@gmail.com>
2020-04-08 02:53:13 +0300

    Adds rewards system configs

--- config.py
+++ config.py
@@ -352,3 +352,7 @@ except KeyError:
     GE_DB_PORT = ''
     GE_DB_PASSWORD = ''
     GE_HTTP_PROVIDER = ''
+
+TRANSFERS_CREATED_SINCE = common_secrets_parser['REWARDS'].getint('created_since')
+ISSUABLE_REWARD_AMOUNT = common_secrets_parser['REWARDS'].getint('issuable_amount')
+TRANSFER_AMOUNT_LIMIT = common_secrets_parser['REWARDS'].getint('transfer_amount')
