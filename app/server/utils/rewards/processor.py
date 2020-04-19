d8e8bb02e582f9506cf3a9a70f012a53d85d324f
Philip Wafula <philipwafula2@gmail.com>
2020-04-19 09:56:09 +0300

    Refactors processor
    - Changes method to get token from indexed column using token id retrieved from query.
    - Gets single disbursing admin to use for all disbursements.
    - Adds list of supported REWARDS_DATA_TAGS
    - Adds logging for transactions.
    -Adds persistence for disbursement failure points to provide a return checkpoint when issuing daily bonuses.

--- app/server/utils/rewards/processor.py
+++ app/server/utils/rewards/processor.py
@@ -1,13 +1,20 @@
+import logging
+
 from flask import g
+
+logging.basicConfig(level=logging.DEBUG)
+rewards_logger = logging.getLogger()
 
 from server import db
 from server.models.user import User
 from server.models.reward import Reward
-from server.models.transfer_account import TransferAccount
+from server.models.organisation import Organisation
 from server.models.token import Token
 from server.utils.credit_transfer import make_payment_transfer
 from server.utils.transfer_enums import TransferSubTypeEnum
 from server.utils.rewards.queries import users_with_corresponding_total_outward_txs
+
+REWARD_DATA_TAGS = ['DAILY_BONUS', 'FAILURE_POINT']
 
 
 def get_user_by_id(user_id: int):
@@ -23,14 +30,12 @@ def get_user_by_id(user_id: int):
         raise Exception('User with id {} not found'.format(user_id))
 
 
-def get_transfer_account_token(transfer_account: TransferAccount):
+def get_user_token(token_id: int):
     """
     Gets the token for a specific transfer account
-    :param transfer_account: transfer account object
+    :param token_id: user's token id
     :return: token object
     """
-
-    token_id = transfer_account.token_id
     token = Token.query.get(token_id)
 
     if token:
@@ -39,18 +44,16 @@ def get_transfer_account_token(transfer_account: TransferAccount):
         raise Exception('No token found for token id {}'.format(token_id))
 
 
-def get_admin_account_to_disburse(subject_user: User):
+def get_admin_account_to_disburse():
     """
-    This method gets all admin users for a specific organization and uses the first admin account to disburse a specific
+    This method gets all admin users for a specific organisation and uses the first admin account to disburse a specific
     amount.
-    :param subject_user:
-    :return: user object with admin role
     """
     # initialize organisation admins
     organisation_admins = []
 
     # get a user's default organisation
-    user_organisation = subject_user.default_organisation
+    user_organisation = Organisation.master_organisation()
 
     # iterates through the organisation's users to find admins
     for organisation_user in user_organisation.users:
@@ -62,10 +65,7 @@ def get_admin_account_to_disburse(subject_user: User):
         admin = organisation_admins[0]
         return admin
     else:
-        raise ValueError('The provided user {}{} seems to have no admin users in parent organization.'.format(
-            subject_user.first_name,
-            subject_user.last_name
-        ))
+        raise ValueError('No admin users found to disburse.')
 
 
 def persist_reward_data(recipients_data: dict, tag: str):
@@ -74,6 +74,9 @@ def persist_reward_data(recipients_data: dict, tag: str):
     :param recipients_data:
     :param tag:
     """
+    if tag not in REWARD_DATA_TAGS:
+        raise ValueError('Unsupported reward data tag.')
+
     reward = Reward(tag=tag)
     db.session.add(reward)
     reward.enter_recipient_data(recipients_data)
@@ -97,6 +100,7 @@ def process_daily_bonus_query(daily_bonus_query_result: list):
     processed_user_ids_with_total_unique_outward_transactions = []
 
     for counter, (user_id,
+                  token_id,
                   user_total_unique_outward_txs) in enumerate(daily_bonus_query_result):
         # compute total unique outward transactions for all users
         collective_unique_outward_transactions += user_total_unique_outward_txs
@@ -110,19 +114,12 @@ def process_daily_bonus_query(daily_bonus_query_result: list):
         # find user by user id
         user = get_user_by_id(user_id)
 
-        # define user transfer account
-        user_transfer_account = user.default_transfer_account
-
         # get user's token
-        user_token = get_transfer_account_token(user_transfer_account)
-
-        # get disbursing admin
-        disbursing_admin = get_admin_account_to_disburse(user)
+        user_token = get_user_token(token_id=token_id)
 
         # add tuple of user and corresponding total unique outward transaction to list
         user_and_total_unique_outward_transactions_list.append((user,
                                                                 user_total_unique_outward_txs,
-                                                                disbursing_admin,
                                                                 user_token))
 
     # persist reward data
@@ -131,7 +128,7 @@ def process_daily_bonus_query(daily_bonus_query_result: list):
         'collective_unique_outward_txs': collective_unique_outward_transactions
     }
 
-    persist_reward_data(recipients_data=recipient_data, tag='Daily bonus')
+    persist_reward_data(recipients_data=recipient_data, tag='DAILY_BONUS')
 
     return user_and_total_unique_outward_transactions_list, collective_unique_outward_transactions
 
@@ -143,29 +140,50 @@ def make_daily_bonus_disbursements(created_since: int, issuable_amount: int, tra
     user_and_total_unique_outward_txs_list, collective_unique_outward_txs = process_daily_bonus_query(
         daily_bonus_query_result=daily_bonus_query_result)
 
-    # iterate through list to make disbursements
-    for counter, \
-        (user,
-         unique_outward_transactions_per_user,
-         disbursing_admin,
-         user_token) in enumerate(user_and_total_unique_outward_txs_list):
-        # compute user's total unique outward transactions as a percentage of the collective unique outward
-        # transactions
-        user_percentage_unique_outward_transactions = (
-            (unique_outward_transactions_per_user / collective_unique_outward_txs))
+    # get disbursing admin
+    disbursing_admin = get_admin_account_to_disburse()
 
-        # get user's share of the total issuable amount
-        user_bonus_amount = int(user_percentage_unique_outward_transactions * issuable_amount)
+    try:
+        # iterate through list to make disbursements
+        for counter, (user,
+                      unique_outward_transactions_per_user,
+                      user_token) in enumerate(user_and_total_unique_outward_txs_list):
+            # compute user's total unique outward transactions as a percentage of the collective unique outward
+            # transactions
+            user_percentage_unique_outward_transactions = (
+                (unique_outward_transactions_per_user / collective_unique_outward_txs))
 
-        if user_bonus_amount > 1:
-            disbursement = make_payment_transfer(user_bonus_amount,
-                                                 receive_user=user,
-                                                 send_user=disbursing_admin,
-                                                 transfer_subtype=TransferSubTypeEnum.DISBURSEMENT,
-                                                 token=user_token)
+            # get user's share of the total issuable amount
+            user_bonus_amount = int(user_percentage_unique_outward_transactions * issuable_amount)
 
-            db.session.add(disbursement)
-            db.session.commit()
+            if user_bonus_amount > 1:
+                try:
+                    disbursement = make_payment_transfer(user_bonus_amount,
+                                                         send_user=disbursing_admin,
+                                                         receive_user=user,
+                                                         transfer_subtype=TransferSubTypeEnum.DISBURSEMENT,
+                                                         token=user_token)
+
+                    db.session.add(disbursement)
+                    db.session.commit()
+
+                    rewards_logger.info('DISBURSING: {} TO USER_ID: {}'.format(user_bonus_amount,
+                                                                               user.id))
+                except Exception as exception:
+                    rewards_logger.error(
+                        'DISBURSEMENT FAILED FOR USER_ID: {}, WITH ERROR: {}'.format(user.id,
+                                                                                     exception))
+                    disbursement_failure_point = {
+                        'user_id': user.id,
+                        'user_bonus_amount': user_bonus_amount
+                    }
+
+                    # persist failure point
+                    persist_reward_data(recipients_data=disbursement_failure_point,
+                                        tag='FAILURE_POINT')
+
+    except Exception as exception:
+        rewards_logger.error('AN ERROR OCCURRED: {}'.format(exception))
 
 
 class RewardsProcessor:
