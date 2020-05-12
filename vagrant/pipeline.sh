e148e16006fb32c317cd5ce9f420fc7adabdd62f
Benjamin Scholtz <contact@benjaminscholtz.com>
2020-05-12 15:14:45 +0200

    Update pipeline

--- vagrant/pipeline.sh
+++ vagrant/pipeline.sh
@@ -2,11 +2,19 @@
 
 set -ex
 
+source .envrc
+
+[ -d '../test' ] && rm -R ../test
+git clone . ../test
+pushd ../test
+
 ./vagrant/generate_secrets.sh
 ./vagrant/node_install.sh
 ./vagrant/node_build.sh
 ./vagrant/docker_build.sh
 ./vagrant/test_backend.sh
 ./vagrant/test_frontend.sh
+
+popd
 
 set +ex
