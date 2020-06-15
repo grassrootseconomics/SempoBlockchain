6a4f4849608aee1222ec65e1fae45eb3f600d740
nolash <dev@holbrook.no>
2020-06-15 12:38:21 +0200

    Add null legacy location entry api

--- app/server/api2/geolocation_legacy_api.py
+++ app/server/api2/geolocation_legacy_api.py
@@ -20,15 +20,36 @@ class GetLegacyLocation(MethodView):
         """
         user = User.query.get(user_id)
         response_object = {
-                "lat": user.lat,
-                "lng": user.lng,
-                "location": user._location,
+                'data': {
+                    'lat': user.lat,
+                    'lng': user.lng,
+                    'location': user._location,
+                    },
                 }
         return make_response(jsonify(response_object)), 200
+
+    def delete(self, user_id):
+        """Nullifies legacy location information for a user
+        """
+        user = User.query.get(user_id)
+        user._location = None
+        db.session.add(user)
+        db.session.commit()
+        response_object = {
+            'message': 'legacy location nulled',
+                }
+        return make_response(jsonify(response_object)), 204
+
 
 
 geolocation_legacy_blueprint.add_url_rule(
         '/geolocation/legacy/user/<int:user_id>/',
     view_func=GetLegacyLocation.as_view('v2_geolocation_legacy_user_view'),
     methods=['GET']
+)
+
+geolocation_legacy_blueprint.add_url_rule(
+        '/geolocation/legacy/user/<int:user_id>/',
+    view_func=GetLegacyLocation.as_view('v2_geolocation_legacy_user_delete_view'),
+    methods=['DELETE']
 )
